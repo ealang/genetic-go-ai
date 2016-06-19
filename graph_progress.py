18c04cca76d563a26394ddc04553f5d32f9d7fc8
eric <ealang@gmail.com>
2016-06-18 20:19:04 -0500

    Add ability to compare multiple log files

--- graph_progress.py
+++ graph_progress.py
@@ -1,12 +1,6 @@
-import re
-import sys
-from collections import defaultdict
-from matplotlib import pyplot
-
-
 class Series:
-    def __init__(self, offset):
-        self._offset = offset
+    def __init__(self):
+        from collections import defaultdict
         self._icount = defaultdict(int)
         self._isum = defaultdict(float)
         self.data_x = []
@@ -15,7 +9,7 @@ class Series:
     def push(self, i, score):
         self._icount[i] += 1
         self._isum[i] += score
-        self.data_x.append(i + self._offset)
+        self.data_x.append(i)
         self.data_y.append(score)
 
     @property
@@ -26,20 +20,45 @@ class Series:
     def trend_y(self):
         return [self._isum[i] / self._icount[i] for i in self.trend_x]
 
-benchmark = Series(0.05)
-generated = Series(-0.05)
-for line in sys.stdin:
-    if line.startswith("training_data: "):
-        i, ai_score, b_score = re.match(r'^training_data: gen=(\d+) ai=\d+ \(size=\d+ depth=\d+\) ai score=(\d+) benchmark score=(\d+)$', line).groups()
-        benchmark.push(int(i), float(b_score))
-        generated.push(int(i), float(ai_score))
 
-pyplot.plot(generated.data_x, generated.data_y, 'og',
-            benchmark.data_x, benchmark.data_y, 'or',
-            generated.trend_x, generated.trend_y, 'g',
-            benchmark.trend_x, benchmark.trend_y, 'r',
-            linewidth=2)
-pyplot.xlabel('iteration')
-pyplot.ylabel('relative score')
-pyplot.legend(['genetic ai', 'benchmark ai'])
-pyplot.show()
+def main():
+    import sys
+    from matplotlib import pyplot
+    from itertools import cycle
+
+    def parse_lines(line_iter):
+        import re
+        for line in line_iter:
+            if line.startswith("training_data: "):
+                i, ai_score, b_score = re.match(r'^training_data: gen=(\d+) ai=\d+ \(size=\d+ depth=\d+\) ai score=(-?\d+) benchmark score=(-?\d+)$', line).groups()
+                yield (int(i), float(ai_score), float(b_score))
+
+    def load_series(series, line_iter):
+        for i, ai_score, b_score in parse_lines(line_iter):
+            series.push(i, ai_score - b_score)
+
+    def plot_series(series, color_code, label=None):
+        pyplot.plot(series.data_x, series.data_y, '.' + color_code)
+        pyplot.plot(series.trend_x, series.trend_y, color_code, linewidth=2, label=label)
+
+    color_codes = cycle(['b', 'r', 'g', 'm', 'c', 'k'])
+    if len(sys.argv) == 1:
+        series = Series()
+        load_series(series, sys.stdin)
+        plot_series(series, color_codes[0])
+
+    else:
+        filenames = sys.argv[1:]
+        series_list = [Series() for _ in filenames]
+        for series, filename, color_code in zip(series_list, filenames, color_codes):
+            with open(filename) as fd:
+                load_series(series, fd)
+            plot_series(series, color_code, filename)
+
+    pyplot.legend()
+    pyplot.xlabel('iteration')
+    pyplot.ylabel('performance')
+    pyplot.show()
+
+if __name__ == '__main__':
+    main()
