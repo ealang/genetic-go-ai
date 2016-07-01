c12532171aef7361b4aec730507169ff6bd3e6c2
ealang <ealang@gmail.com>
2016-07-01 06:05:25 +0000

    Smaller tasks for better cpu utilization

--- src/evolution/scoring_algs.cpp
+++ src/evolution/scoring_algs.cpp
@@ -1,5 +1,3 @@
-#include <algorithm>
-#include <cassert>
 #include "scoring_algs.h"
 #include "aimatch.h"
 #include "gpnode.h"
@@ -7,49 +5,50 @@
 
 using namespace std;
 
+int maxTurnsForGame(int boardSize) {
+    return int(boardSize * boardSize * 0.7);
+}
+
 BenchmarkScore::BenchmarkScore(const GPNode* benchmark): benchmark(benchmark) {}
 
 unordered_map<int, float> BenchmarkScore::operator()(int boardSize, const vector<const GPNode*>& pop) {
-
     for (unsigned int ai = 0; ai < pop.size(); ai++) {
-        queue.submit([ai, boardSize, &pop, this](){
-            int maxTurnsPerGame = int(boardSize * boardSize * 0.8);
-            int totalScore = 0;
-            for (int game = 0; game < gamesPerEvaluation; game++) {
-                auto result = playAIMatch(*benchmark, *pop[ai], (game % 2) ? BLACK : WHITE, boardSize, maxTurnsPerGame);
-                totalScore += result.whiteScore - result.blackScore;
-            }
-            return Result{(int)ai, (float)totalScore / gamesPerEvaluation};
-        });
+        for (int game = 0; game < gamesPerEvaluation; game++) {
+            queue.submit([ai, boardSize, game, &pop, this](){
+                auto result = playAIMatch(*benchmark, *pop[ai],
+                                          (game % 2) ? BLACK : WHITE,
+                                          boardSize, maxTurnsForGame(boardSize));
+                float score = result.whiteScore - result.blackScore;
+                return make_pair((int)ai, (float)score);
+            });
+        }
     }
 
     unordered_map<int, float> scores;
-    for (unsigned int i = 0; i < pop.size(); i++) {
-        Result result = queue.get();
-        scores.emplace(result.ai, result.score);
+    for (unsigned int i = 0; i < pop.size() * gamesPerEvaluation; i++) {
+        auto result = queue.get();
+        scores[result.first] += result.second / gamesPerEvaluation;
     }
     return scores;
 }
 
 std::unordered_map<int, float> CompetitiveScore::operator()(int boardSize, const std::vector<const GPNode*>& pop) {
     for (unsigned int ai = 0; ai < pop.size(); ai++) {
-        queue.submit([ai, boardSize, &pop, this](){
-            int maxTurnsPerGame = int(boardSize * boardSize * 0.8);
-
-            int totalScore = 0;
-            for (int game = 0; game < gamesPerEvaluation; game++) {
+        for (int game = 0; game < gamesPerEvaluation; game++) {
+            queue.submit([ai, boardSize, game, &pop, this](){
                 auto result = playAIMatch(*pop[rand() % pop.size()], *pop[ai],
                                           (game % 2) ? BLACK : WHITE,
-                                          boardSize, maxTurnsPerGame);
-                totalScore += result.whiteScore - result.blackScore;
-            }
-            return (float)totalScore / gamesPerEvaluation;
-        });
+                                          boardSize, maxTurnsForGame(boardSize));
+                float score = result.whiteScore - result.blackScore;
+                return make_pair((int)ai, (float)score);
+            });
+        }
     }
 
     unordered_map<int, float> scores;
-    for (unsigned int ai = 0; ai < pop.size(); ai++) {
-        scores.emplace(ai, queue.get());
+    for (unsigned int ai = 0; ai < pop.size() * gamesPerEvaluation; ai++) {
+        auto result = queue.get();
+        scores[result.first] += result.second / gamesPerEvaluation;
     }
     return scores;
 }
--- src/evolution/scoring_algs.h
+++ src/evolution/scoring_algs.h
@@ -2,20 +2,16 @@
 #define SCORING_ALGS_H
 
 #include "task_queue.h"
-#include <functional>
 #include <unordered_map>
+#include <utility>
 #include <vector>
 
 class GPNode;
 
 // Score by playing against a benchmark ai
 class BenchmarkScore {
-    static const int gamesPerEvaluation = 20;
-    struct Result {
-        int ai;
-        float score;
-    };
-    TaskQueue<Result> queue;
+    static const int gamesPerEvaluation = 30;
+    TaskQueue<std::pair<int, float>> queue;
     const GPNode* benchmark;
 public:
     BenchmarkScore(const GPNode* benchmark);
@@ -25,7 +21,7 @@ public:
 // Score by playing ai against each other
 class CompetitiveScore {
     static const int gamesPerEvaluation = 30;
-    TaskQueue<float> queue;
+    TaskQueue<std::pair<int, float>> queue;
 public:
     std::unordered_map<int, float> operator()(int boardSize, const std::vector<const GPNode*>& pop);
 };
